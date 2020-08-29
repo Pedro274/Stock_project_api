a56d4190b13a41720239bb496a0e8e29a1f3d9a5
Pedro274 <Pedro.lin.ka@gmail.com>
2020-08-29 02:45:36 +0000

    final_project api funtional version

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Portfolio
 #from models import Person
 
 app = Flask(__name__)
@@ -20,26 +20,103 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+#This endpoint will create a new user and save it in the database (the user will need to send username, password and email in the body of the request)
+@app.route('/register_user', methods=['POST'])
+def handle_register_user():
+    request_data= request.get_json()
+    user = User.query.filter_by(username=request_data['username']).first()
+    if user:
+        return jsonify({'message': 'username already exists'}), 409
+    new_user = User(
+        username=request_data["username"],
+        email = request_data["email"],
+        password = request_data["password"],
+        is_active = True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({
+        "msg": "User was created"
+    }), 200
 
-# this only runs if `$ python src/main.py` is executed
+
+#This endpoint gets all the users from the data base
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users_query = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users_query))
+    return jsonify({
+        "message": "This are all the available user",
+        "users": all_users
+    }), 200
+
+
+#This endpoint gets one user (the user will need to send username and password in the body of the request)
+@app.route('/login', methods=['POST'])
+def handle_login():
+    request_data = request.get_json()
+    email = request_data["email"]
+    users_query = User.query.filter_by(email=email).first()
+    if users_query:
+        return jsonify(users_query.serialize()), 200 
+    return jsonify({"Message":"User not found"}), 404
+
+
+
+#This endpoint will create a new portfolio (user need to provide symbol, companyName, price, shares and totalReturn in the body of the request ), the user_id needs to be in the url
+@app.route('/portfolio/<user_id>', methods=['POST'])
+def post_portfolio(user_id):
+    request_data= request.get_json()
+    stock = Portfolio.query.filter_by(companyName=request_data["companyName"]).filter_by(user_id=user_id).first()
+    if stock:
+        return jsonify({"message": "user already bought this stock"})
+    stock_added= Portfolio(
+        symbol = request_data["symbol"],
+        companyName = request_data["companyName"],
+        price = request_data["price"],
+        shares = request_data["shares"],
+        totalReturn = request_data["totalReturn"],
+        user_id = user_id 
+    )
+    db.session.add(stock_added)
+    db.session.commit()
+    return jsonify({
+        "msg": "Stock Added "
+    }), 201
+
+
+#This endpoint will find one specific portfolio by id (include the portfolio id in the url)
+@app.route('/portfolio/<id>', methods=['GET'])
+def get_portfolio(id):
+    user_portfolio= Portfolio.query.filter_by(id=id).first()
+    if user_portfolio:
+        return jsonify({"portfolio": user_portfolio.serialize()}), 200
+    return jsonify({"message": "Add stocks to your portfolio"}), 404
+        
+
+#This endpoint will find one specific portfolio by id and delete it (include the portfolio id in the url)
+@app.route('/portfolio/<id>', methods=['DELETE'])
+def handle_portfolio(id):
+    stock_sold = Portfolio.query.filter_by(id=id).first()
+    if stock_sold:
+        db.session.delete(stock_sold)
+        db.session.commit()
+        return jsonify({
+            "msg": "Stock Sold "
+        }), 200
+    return jsonify({"message":"Stock not found" }), 404
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,12 +1,19 @@
-from flask_sqlalchemy import SQLAlchemy
 
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__="user"
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), default=False)
+
+    #relationship
+    portfolios = db.relationship('Portfolio', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +21,79 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "password": self.password,
+            "is_active": self.is_active,
+            "portfolio": [portfolio.serialize() for portfolio in self.portfolios.all()]
             # do not serialize the password, its a security breach
         }
+
+class Portfolio(db.Model):
+    __tablename__ = "portfolio"
+
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(120), unique=False, nullable=False)
+    companyName = db.Column(db.String(120), unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    shares = db.Column(db.Integer, unique=False, nullable=False)
+    totalReturn = db.Column(db.Integer, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    #relationship
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return '<Portfolio %r>' % self.companyName
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "symbol": self.symbol,
+            "companyName":self.companyName,
+            "price":self.price,
+            "shares":self.shares,
+            "totalReturn":self.totalReturn
+        }
+
+
+
+
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+
+# class Stock(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     symbol = db.Column(db.String(120), unique=False, nullable=False)
+#     companyName = db.Column(db.String(120), unique=False, nullable=False)
+#     price = db.Column(db.Integer, unique=False, nullable=False)
+#     # password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return '<Stock %r>' % self.symbol
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "symbol": self.symbol,
+#             "companyName":self.companyName,
+#             "price":self.price
+#             # do not serialize the password, its a security breach
+#         }
